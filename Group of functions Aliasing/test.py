fe5a0a61f91d2d573d91a6b55ebfc93670e20e39
unknown <praveenlavudya56@gmail.com>
2023-09-05 09:24:42 +0530

    my programs

---
+++ Group of functions Aliasing/test.py
@@ -0,0 +1,14 @@
+# import praveenmath
+# print(praveenmath.x)
+# praveenmath.add(10,20)
+# praveenmath.product(3,9)
+#---------------------------
+# import praveenmath as M
+# print(M.x)
+# M.add(10,20)
+# M.product(3,9)
+#------------------------
+# from praveenmath import*
+# print(x)
+# add(10,20)
+# product(12,13)
