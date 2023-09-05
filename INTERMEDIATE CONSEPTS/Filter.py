3c1ac01f01ada5cfa4c438bbab89c7f5da7c383b
unknown <praveenlavudya56@gmail.com>
2023-09-05 09:27:10 +0530

    my commit

--- INTERMEDIATE CONSEPTS/Filter.py
+++ INTERMEDIATE CONSEPTS/Filter.py
@@ -1,10 +1,19 @@
-def checkEven(number):
-    return number % 2==0
+#with out lambda function
+# def checkEven(number):
+#     return number % 2==0
 
-arr1=[1,2,3,4,5,6,7,8,9,10]
-even=filter(checkEven,arr1)#map
+# arr1=[1,2,3,4,5,6,7,8,9,10]
+# even=filter(checkEven,arr1)#map
 
-evenArray=[]
-for i in even:
-    evenArray.append(i)
-    print(evenArray)
+# evenArray=[]
+# for i in even:
+#     evenArray.append(i)
+#     print(evenArray)
+
+#----------------------------------------
+#Even ,odd
+l=[0,5,10,15,20,25,30]
+l1=list(filter(lambda x:x%2==0,l))
+print(l1)
+l2=list(filter(lambda x:x%2!=0,l))
+print(l2)
--- INTERMEDIATE CONSEPTS/lambda function.py
+++ INTERMEDIATE CONSEPTS/lambda function.py
@@ -1,3 +1,21 @@
 # square=lambda x:x*x
 # print(square(5))
 
+'''
+s=lambda n:n*n
+print("The Square of 4 is:",s(4))
+print("The Square of 3 is :",s(3))
+'''
+
+'''
+s=lambda a,b:a+b
+print("The sum of 10+20:",s(10,20))
+print("The sum of 100+200:",s(100,200))
+'''
+
+'''
+s=lambda a,b:a if a>b else b
+print("The Biggest of 10,20 is:",s(10,20))
+print("The Biggest of 100,200 is:",s(1000,200))
+'''
+
