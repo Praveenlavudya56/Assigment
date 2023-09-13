d8cc10dfb7229be8f4cc5d1813829179b3c7ce06
unknown <praveenlavudya56@gmail.com>
2023-09-13 13:06:28 +0530

    my commit

--- SortingAlgorithms/Bubble Sort.py
+++ SortingAlgorithms/Bubble Sort.py
@@ -1,14 +1,14 @@
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+# def bubble_sort(arr):
+#     n = len(arr)
+#     for i in range(n):
+#         for j in range(0, n-i-1):
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
-# Example usage
-arr = [64, 34, 25, 12, 22, 11, 90]
-bubble_sort(arr)
-print("Sorted array:", arr)
+# # Example usage
+# arr = [64, 34, 25, 12, 22, 11, 90]
+# bubble_sort(arr)
+# print("Sorted array:", arr)
 #------------------------------------------------------------------------
 def bubbleSort(arr,l):
     for i in range(l):
